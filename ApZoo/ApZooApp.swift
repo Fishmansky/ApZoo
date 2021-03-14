8724c1962e45956ec7bea06c2aa48050f4484ca4
FISHMANSKY <paul96guitar@gmail.com>
2021-03-14 15:38:57 +0100

    Initial Commit

---
+++ ApZoo/ApZooApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApZooApp.swift
+//  ApZoo
+//
+//  Created by Mac on 14/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct ApZooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

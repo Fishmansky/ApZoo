bca31e7eed6cdeec6150ac757a93dc1c550d35ef
FISHMANSKY <paul96guitar@gmail.com>
2021-03-28 15:43:07 +0200

    First Iteration (Worker's List visible on-screen)

--- ApZoo/ApZooApp.swift
+++ ApZoo/ApZooApp.swift
@@ -7,6 +7,91 @@
 
 import SwiftUI
 
+enum PriorityStatus {
+    case Low
+    case Medium
+    case High
+}
+
+class Worker: Identifiable {
+    let name: String
+    
+    init (_ n: String){
+        name = n
+    }
+    
+    var Tasks = [Task]()
+    
+    func addTask(_ newTask: Task){
+        Tasks.append(newTask)
+    }
+    
+    func currentTask() -> String{
+        return Tasks.first!.description
+    }
+    func getUserCircle()-> some View{
+        let userCircle = SwiftUI.VStack(content: {
+            Image("Logo2-pic")
+                .resizable()
+                .clipShape(Circle())
+                .shadow(radius: 10)
+                .overlay(Circle().stroke(Color.blue, lineWidth: 3))
+                .frame(width: 40, height: 40, alignment: .center)
+            Text(name)
+        })
+        return userCircle
+    }
+}
+
+class Task: Identifiable {
+    let description: String
+    let Priority: PriorityStatus
+    
+    init (_ desc: String, priority: PriorityStatus){
+        description = desc
+        Priority = priority
+    }
+}
+
+class TaskManager {
+    
+    var WorkerList: [Worker] = []
+    var TaskList: [Task] = []
+    init (){
+        WorkerList.append(Worker("Adam"))
+        WorkerList.append(Worker("Adm"))
+        WorkerList.append(Worker("Ada32"))
+        TaskList.append(Task("Do the vaccuming", priority: .High))
+        TaskList.append(Task("Do the dishes", priority: .Medium))
+        TaskList.append(Task("Check water supply", priority: .Low))
+        
+        
+        WorkerList[0].addTask(TaskList[0])
+        WorkerList[1].addTask(TaskList[1])
+        WorkerList[2].addTask(TaskList[2])
+    }
+    
+    func getWorkerList() -> [String]{
+        var StringList = [String]()
+        for worker in WorkerList {
+            StringList.append(worker.name)
+        }
+        return StringList
+    }
+    
+    func getTaskList() -> [String]{
+        var StringList = [String]()
+        for task in TaskList {
+            StringList.append(task.description)
+        }
+        return StringList
+    }
+    
+
+}
+
+var TM = TaskManager()
+
 @main
 struct ApZooApp: App {
     var body: some Scene {
--- ApZoo/ContentView.swift
+++ ApZoo/ContentView.swift
@@ -7,10 +7,79 @@
 
 import SwiftUI
 
+struct WorkersView: View {
+    var body: some View {
+        List(TM.WorkerList) { worker in
+                VStack(alignment: .leading) {
+                        Text(worker.name)
+                    Text("Currently working on: \(worker.currentTask())")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    
+                }
+            }
+    }
+}
+
+struct TasksView: View {
+    var body: some View {
+        let layoutWorkers: [GridItem] = [GridItem(.flexible())]
+        let layoutTasks: [GridItem] = [GridItem(.flexible()), GridItem(.flexible())]
+        let colors = [ PriorityStatus.High : Color.red,
+                       PriorityStatus.Medium : Color.yellow,
+                       PriorityStatus.Low : Color.green]
+        VStack{
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: layoutWorkers, content: {
+                    ForEach((0..<TM.WorkerList.count), id: \.self) {
+                        TM.WorkerList[$0].getUserCircle()
+                    }
+                })
+            }
+            .frame(height: 100)
+            ScrollView {
+                LazyVGrid(columns: layoutTasks, spacing: 20) {
+                    ForEach((0..<TM.TaskList.count), id: \.self) {
+                        Text(TM.getTaskList() [$0 % TM.TaskList.count])
+                            .font(.system(size: 20))
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 80)
+                            .background(colors[TM.TaskList[$0].Priority])
+                            .cornerRadius(10)
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center){
+            HStack(){
+                Spacer()
+                Image("Logo2-text")
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+                Image("Logo2-pic")
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+            }
+            .scaledToFit()
+            TabView{
+                WorkersView()
+                    .tabItem {
+                        Label("Workers", systemImage: "list.dash")
+                    }
+                TasksView()
+                    .tabItem {
+                        Label("Tasks", systemImage: "list.dash")
+                    }
+            }
+        }
+        
     }
 }
 
